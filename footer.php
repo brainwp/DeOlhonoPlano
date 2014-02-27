ecaedd0b1ce3e7ddb1debd2f1fbb23749d713434
unknown <matiaseveraldo@gmail.com>
2014-02-27 13:02:30 -0300

    Alterando frase do Rodape.

--- footer.php
+++ footer.php
@@ -1,19 +1,9 @@
 			<!-- </div> 	<!-- end #main-area -->
-
-    </div><!-- .content-home -->
-    </div><!-- .container -->
-
+    </div><!-- .content-home -->    </div><!-- .container -->
 		<footer id="main_footer">
-			<p id="copyright"><?php printf( __('Desenvolvido por %s | Editado por %s', 'Flexible'), '<a href="http://www.elegantthemes.com" title="Premium WordPress Themes">Elegant Themes</a>', '<a href="http://www.acaoeducativa.org.br/">A&ccedil;&atilde;o Educativa</a>' ); ?></p>
-            <div class="brasa">
-            	<a href="http://www.brasa.art.br" target="_blank"></a>
-            </div><!-- .brasa -->
+			<p id="copyright">				<?php echo get_bloginfo('name'); ?> | Sob uma Licença <a href="http://creativecommons.org.br/" target="_blank">Creative Commons</a> | <a href="<?php echo home_url('creditos'); ?>">Créditos</a> | Desenvolvido com <a href="http://br.wordpress.org/" target="_blank">WordPress</a>			</p>
+            <div class="brasa">            	<a href="http://www.brasa.art.br" target="_blank"></a>            </div><!-- .brasa -->
 		</footer> <!-- end #main_footer -->
-
-	<div class="container">
-
-	</div> <!-- end #container -->
-
+	<div class="container">	</div> <!-- end #container -->
 	<?php wp_footer(); ?>
-</body>
-</html>
+</body></html>
